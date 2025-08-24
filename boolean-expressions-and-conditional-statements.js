50817c78f9017eccccc3a310790c9da7c8d1eea3
Rebecca Kunce <rebeccakunce@gmail.com>
2025-08-24 03:38:35 -0500

    boolean expressions and conditional statements Becca K

--- boolean-expressions-and-conditional-statements.js
+++ boolean-expressions-and-conditional-statements.js
@@ -29,6 +29,7 @@ const readline = require('readline-sync');
 const hasTorch = true;
 const hasMap = false;
 
+
 console.log("You see two paths: one leads to the mountains, the other to the village.");
 const choice = readline.question("Do you go to the 'mountains' or the 'village'?");
 
@@ -41,6 +42,31 @@ if (choice === "mountains" && hasTorch) {
 } else {
   console.log("You get lost and wander aimlessly.");
 }
+
+const hasCompass = true;
+const creepyMan = false;
+
+console.log("You find a stray dog. She seems friendly, healthy, and sweet.");
+const dogChoice = readline.question("Do you want to 'befriend' the dog or 'leave it alone'?");
+
+
+
+ 
+
+if (dogChoice === "befriend" && !creepyMan) {
+  console.log("A creepy man appeared on the path but your new found pup chased him away.");
+} else if (dogChoice === "leave it alone" && !creepyMan) {
+  console.log("A creepy man appeared on the path and you haven't been heard from since.");
+} else if (dogChoice === "befriend" || hasCompass) {
+  console.log("You are guided back to your home!");
+} else if (dogChoice === "leave it alone" && !hasCompass) {
+  console.log("You get lost and are never heard from again.");
+}
+  else {
+  console.log ("Your trip was pretty boring but you made it home.");
+}
+
+
 
 /* 
 
